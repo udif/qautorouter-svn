b5e8a05704b716032b3785552f6a94f0efef6eea
eightbitgeek <eightbitgeek@54a98769-9030-4c90-a79a-882ed139499d>
2011-06-26 22:41:02 +0000

    Little bit of work on CGSegment class.

--- cgsegment.cpp
+++ cgsegment.cpp
@@ -4,6 +4,8 @@
 *******************************************************************************/
 #include "cgsegment.h"
 
+#include <QPainter>
+
 #define inherited QObject
 
 CGSegment::CGSegment(QObject *parent)
@@ -15,9 +17,23 @@ CGSegment::~CGSegment()
 {
 }
 
-QPainterPath CGSegment::shape()
+QRectF CGSegment::boundingRect() const
+{
+	QRectF bounds = shape().boundingRect();
+	return bounds;
+}
+
+QPainterPath CGSegment::shape() const
 {
 	QPainterPath ppath;
-	// FIXME - do stuff here
 	return ppath;
+}
+
+void CGSegment::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+	QPainterPath p = shape();
+	painter->setRenderHint(QPainter::Antialiasing);
+	painter->scale(scale(),scale());
+	painter->setPen(QPen(QColor(255, 255, 255), 3, Qt::SolidLine,Qt::FlatCap,Qt::MiterJoin));
+	painter->drawPath(shape());
 }
--- include/cgsegment.h
+++ include/cgsegment.h
@@ -7,15 +7,19 @@
 
 #include <QObject>
 #include <QPainterPath>
+#include <QGraphicsItem>
 
-class CGSegment : public QObject
+class CGSegment : public QObject, public QGraphicsItem
 {
 	Q_OBJECT
 	public:
 		CGSegment(QObject *parent = 0);
 		virtual ~CGSegment();
 
-		virtual QPainterPath			shape();
+		virtual QRectF				boundingRect() const;
+		virtual QPainterPath		shape() const;
+		virtual void				paint(QPainter *painter, const QStyleOptionGraphicsItem *option,QWidget *widget);
+
 };
 
 #endif // CGSEGMENT_H
